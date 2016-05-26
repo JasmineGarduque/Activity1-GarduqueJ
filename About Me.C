59fa4fdb581fac8e96bd90f6d377b51c9e64c489
JasmineGarduque <jasmine_garduque@dlsu.edu.ph>
2016-05-26 17:09:09 +0800

    Create About Me.C

---
+++ About Me.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+	printf("\t\tJasmine Garduque\n");
+	printf("\t\t      CIV\n");
+	printf("\t\t    11432675\n");
+	printf("\t\tSeptember 07, 1995\n");
+	printf("\t      De La Salle University\n");
+	printf("\t\t   Philippines\n");
+	printf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+
+
+
+	return 0;
+}
