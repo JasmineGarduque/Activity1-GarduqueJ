334c36d2bce1b341f36a05c98ae125e0e1b53453
JasmineGarduque <jasmine_garduque@dlsu.edu.ph>
2016-05-26 17:08:01 +0800

    Create Value of x.C

---
+++ Value of x.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	float x, n;
+	printf("Enter the value of x: ");
+	scanf("%f", &x);
+	
+	n=x/70;
+	printf("The value of f(x) is: %f\n", n);
+	
+	return 0;
+}
